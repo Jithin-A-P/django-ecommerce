d160ead9b018409d86ec5ba12f423ff6fe974035
Jithin A P <jithinap009@gmail.com>
2021-02-19 16:12:28 +0530

    Added all product page into urls

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
-from core.views import item_list
 from django.urls import path
-from django.urls import path
-from .views import item_list
+from .views import HomeView, ItemDetailView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list'),
+    path('', HomeView.as_view(), name='home'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
 ]
